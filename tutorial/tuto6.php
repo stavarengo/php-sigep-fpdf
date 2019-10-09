08716a9d61051c941394a0b502ef2fc891283584
Rodrigo Laurindo <rblaurin@gmail.com>
2019-10-09 15:42:22 -0300

    Construtor, comparação estrita, visibilidade

--- tutorial/tuto6.php
+++ tutorial/tuto6.php
@@ -3,101 +3,101 @@ require('../fpdf.php');
 
 class PDF extends FPDF
 {
-var $B;
-var $I;
-var $U;
-var $HREF;
+    public $B;
+    public $I;
+    public $U;
+    public $HREF;
 
-function PDF($orientation='P', $unit='mm', $size='A4')
-{
-	// Call parent constructor
-	$this->FPDF($orientation,$unit,$size);
-	// Initialization
-	$this->B = 0;
-	$this->I = 0;
-	$this->U = 0;
-	$this->HREF = '';
-}
+    public function __construct($orientation = 'P', $unit = 'mm', $size = 'A4')
+    {
+        // Call parent constructor
+        $this->FPDF($orientation, $unit, $size);
+        // Initialization
+        $this->B = 0;
+        $this->I = 0;
+        $this->U = 0;
+        $this->HREF = '';
+    }
 
-function WriteHTML($html)
-{
-	// HTML parser
-	$html = str_replace("\n",' ',$html);
-	$a = preg_split('/<(.*)>/U',$html,-1,PREG_SPLIT_DELIM_CAPTURE);
-	foreach($a as $i=>$e)
-	{
-		if($i%2==0)
-		{
-			// Text
-			if($this->HREF)
-				$this->PutLink($this->HREF,$e);
-			else
-				$this->Write(5,$e);
-		}
-		else
-		{
-			// Tag
-			if($e[0]=='/')
-				$this->CloseTag(strtoupper(substr($e,1)));
-			else
-			{
-				// Extract attributes
-				$a2 = explode(' ',$e);
-				$tag = strtoupper(array_shift($a2));
-				$attr = array();
-				foreach($a2 as $v)
-				{
-					if(preg_match('/([^=]*)=["\']?([^"\']*)/',$v,$a3))
-						$attr[strtoupper($a3[1])] = $a3[2];
-				}
-				$this->OpenTag($tag,$attr);
-			}
-		}
-	}
-}
+    public function WriteHTML($html)
+    {
+        // HTML parser
+        $html = str_replace("\n", ' ', $html);
+        $a = preg_split('/<(.*)>/U', $html, -1, PREG_SPLIT_DELIM_CAPTURE);
+        foreach ($a as $i => $e) {
+            if ($i % 2 == 0) {
+                // Text
+                if ($this->HREF) {
+                    $this->PutLink($this->HREF, $e);
+                }
+                else {
+                    $this->Write(5, $e);
+                }
+            } else if ($e[0] === '/') {
+                $this->CloseTag(strtoupper(substr($e, 1)));
+            }
+            else {
+                // Extract attributes
+                $a2 = explode(' ', $e);
+                $tag = strtoupper(array_shift($a2));
+                $attr = [];
+                foreach ($a2 as $v) {
+                    if (preg_match('/([^=]*)=["\']?([^"\']*)/', $v, $a3)) {
+                        $attr[strtoupper($a3[1])] = $a3[2];
+                    }
+                }
+                $this->OpenTag($tag, $attr);
+            }
+        }
+    }
 
-function OpenTag($tag, $attr)
-{
-	// Opening tag
-	if($tag=='B' || $tag=='I' || $tag=='U')
-		$this->SetStyle($tag,true);
-	if($tag=='A')
-		$this->HREF = $attr['HREF'];
-	if($tag=='BR')
-		$this->Ln(5);
-}
+    public function OpenTag($tag, $attr)
+    {
+        // Opening tag
+        if ($tag === 'B' || $tag === 'I' || $tag === 'U') {
+            $this->SetStyle($tag, true);
+        }
+        if ($tag === 'A') {
+            $this->HREF = $attr['HREF'];
+        }
+        if ($tag === 'BR') {
+            $this->Ln(5);
+        }
+    }
 
-function CloseTag($tag)
-{
-	// Closing tag
-	if($tag=='B' || $tag=='I' || $tag=='U')
-		$this->SetStyle($tag,false);
-	if($tag=='A')
-		$this->HREF = '';
-}
+    public function CloseTag($tag)
+    {
+        // Closing tag
+        if ($tag === 'B' || $tag === 'I' || $tag === 'U') {
+            $this->SetStyle($tag, false);
+        }
+        if ($tag === 'A') {
+            $this->HREF = '';
+        }
+    }
 
-function SetStyle($tag, $enable)
-{
-	// Modify style and select corresponding font
-	$this->$tag += ($enable ? 1 : -1);
-	$style = '';
-	foreach(array('B', 'I', 'U') as $s)
-	{
-		if($this->$s>0)
-			$style .= $s;
-	}
-	$this->SetFont('',$style);
-}
+    public function SetStyle($tag, $enable)
+    {
+        // Modify style and select corresponding font
+        $this->$tag += ($enable ? 1 : -1);
+        $style = '';
+        foreach (['B', 'I', 'U'] as $s) {
+            if ($this->$s > 0) {
+                $style .= $s;
+            }
+        }
+        $this->SetFont('', $style);
+    }
 
-function PutLink($URL, $txt)
-{
-	// Put a hyperlink
-	$this->SetTextColor(0,0,255);
-	$this->SetStyle('U',true);
-	$this->Write(5,$txt,$URL);
-	$this->SetStyle('U',false);
-	$this->SetTextColor(0);
-}
+    public function PutLink($URL, $txt)
+    {
+        // Put a hyperlink
+        $this->SetTextColor(0, 0, 255);
+        $this->SetStyle('U', true);
+        $this->Write(5, $txt, $URL);
+        $this->SetStyle('U', false);
+        $this->SetTextColor(0);
+    }
 }
 
 $html = 'You can now easily print text mixing different styles: <b>bold</b>, <i>italic</i>,
@@ -121,4 +121,3 @@ $pdf->SetLeftMargin(45);
 $pdf->SetFontSize(14);
 $pdf->WriteHTML($html);
 $pdf->Output();
-?>
